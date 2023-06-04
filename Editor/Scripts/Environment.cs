e2c8b9cfe610b2ecf8913fd2b938a65fa095dadb
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-06-04 14:15:35 -0600

    Refactor Environment to use SceneReference instead of prefab reference

--- Editor/Scripts/Environment.cs
+++ Editor/Scripts/Environment.cs
@@ -1,13 +1,23 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace MetaverseSandbox.Core {
+    [Serializable]
+    public class SceneReference : AssetReferenceT<SceneAsset>
+    {
+        public SceneReference(string guid) : base(guid)
+        {
+        }
+    }
+
     [CreateAssetMenu(fileName = "Environment", menuName = "ScriptableObjects/EnvironmentScriptableObject", order = 1)]
     public class Environment : ScriptableObject
     {
-        public AssetReferenceGameObject environmentAddressablePrefab;
+        public SceneReference environmentAddressableScene;
         // Start is called before the first frame update
         void Start()
         {
