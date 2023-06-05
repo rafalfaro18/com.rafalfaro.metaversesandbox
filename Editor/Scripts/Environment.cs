2fc14e85f91de205b253b389962fcaeab21acea0
Rafael Alfaro <rafalfaro_18@outlook.com>
2023-06-04 18:51:20 -0600

    Replace SceneAssetReference with string which works as Addressable key

--- Editor/Scripts/Environment.cs
+++ Editor/Scripts/Environment.cs
@@ -1,23 +1,15 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 
 namespace MetaverseSandbox.Core {
-    [Serializable]
-    public class SceneReference : AssetReferenceT<SceneAsset>
-    {
-        public SceneReference(string guid) : base(guid)
-        {
-        }
-    }
 
     [CreateAssetMenu(fileName = "Environment", menuName = "ScriptableObjects/EnvironmentScriptableObject", order = 1)]
     public class Environment : ScriptableObject
     {
-        public SceneReference environmentAddressableScene;
+        public string environmentAddressableScene;
         // Start is called before the first frame update
         void Start()
         {
